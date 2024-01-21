9d4b5652fcf2c8c737773847e7d10dadbc423fa9
PoliJun <rivenloving@gmail.com>
2024-01-22 05:40:26 +0800

    Refactor events page and events category page

--- pages/events/[cat]/index.js
+++ pages/events/[cat]/index.js
@@ -2,20 +2,20 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 
-const EventsPerCityPage = ({ data }) => {
+const EventsCatPage = ({ data, pageName }) => {
   return (
     <div>
-      <h1>Events in </h1>
+      <h1>Events in {pageName}</h1>
       {data.map((ev) => (
-        <Link href={`/events/${ev.city}/id`} key={ev.id}>
-          <Image width={300} height={300} alt={ev.image} priority={false}/>
+        <Link href={`/events/${ev.city}/${ev.id}`} key={ev.id} passHref>
+          <Image src={ev.image} width={300} height={300} alt={ev.image} priority={true}/>
           <h2>{ev.title}</h2> <p>{ev.description}</p>
         </Link>
       ))}
     </div>
   );
 };
-export default EventsPerCityPage;
+export default EventsCatPage;
 
 // export function getServerSideProps() {
 //   return {
@@ -32,14 +32,14 @@ export async function getStaticProps(context) {
   const data = allEvents.filter((ev) => ev.city === id);
   console.log(data);
   return {
-    props: { data },
+    props: { data, pageName: id },
   };
 }
 export async function getStaticPaths() {
   const { events_categories } = await import('/data/data.json');
 
-  const allPaths = events_categories.map((ev) => ({
-    params: { cat: ev.id.toString() },
+  const allPaths = events_categories.map((ec) => ({
+    params: { cat: ec.id.toString() },
   }));
     console.log(`allPaths:${allPaths}`);
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,23 +1,49 @@
-import Link from "next/link";
+import Link from 'next/link';
+import Image from 'next/image';
 
-const EventsPage = () => (
-  <div>
-    <h1>Events Page</h1>
+const EventsPage = ({ data }) => {
+  return (
     <div>
-      <Link href="/events/London">
-        <img />
-        <h1>Events in London</h1>
-      </Link>
-      <Link href="/events/Manchester">
-        <img />
-        <h1>Events in Manchester</h1>
-      </Link>
-      <Link href="/events/Liverpool">
-        <img />
-        <h1>Events in Liverpool</h1>
-      </Link>
+        {console.log('Fetching data from getServerSideProps')}
+        {console.log('Fetching data from getServerSideProps')}
+        {console.log('Fetching data from getServerSideProps')}
+        {console.log('Fetching data from getServerSideProps')}
+        {console.log('Fetching data from getServerSideProps')}
+        {console.log('Fetching data from getServerSideProps')}
+      <h1>Events Page</h1>
+      {data.map((ec) => (
+        <Link href={`/events/${ec.id}`} key={ec.id}>
+          <Image
+            src={ec.image}
+            width={300}
+            height={300}
+            alt={ec.id}
+            priority={true}
+          />
+          <h2>{ec.title}</h2> <p>{ec.description}</p>
+        </Link>
+      ))}
     </div>
-  </div>
-);
+  );
+};
 export default EventsPage;
 
+export async function getServerSideProps() {
+  // const data = await import("../data.json");
+  try {
+    const { events_categories } = await import('/data/data.json');
+    return {
+      props: {
+        title: 'Hello everyone!',
+        data: events_categories,
+      },
+    };
+  } catch (e) {
+    console.error('error occured', e);
+    return {
+      props: {
+        title: 'Hello everyone!',
+      },
+    };
+  }
+}
