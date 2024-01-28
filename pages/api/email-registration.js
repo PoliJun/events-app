292582ad8d1ddf8685aedc9e1892c8e05e03be2f
PoliJun <rivenloving@gmail.com>
2024-01-28 23:20:00 +0800

    Add email registration functionality to API and single event page

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -1,3 +1,54 @@
+import path from 'path';
+import fs from 'fs';
+
+function buildPath() {
+  return path.join(process.cwd(), 'data', 'data.json');
+}
+
+function extractData(filePath) {
+  const jsonData = fs.readFileSync(filePath);
+  return JSON.parse(jsonData);
+}
+
 export default function handler(req, res) {
-  
+  const { method } = req;
+  // Access our data
+  // Extract our data (AllEvents)
+  // res 404 if there are no AllEvents
+  // AllEvents - loop through them and identify the event ID
+  // Add the email into emails registered
+  // only if that email doesn't exist
+  // check the format of the email is ok
+
+  const filePath = buildPath();
+  const { events_categories, allEvents } = extractData(filePath);
+
+  if (!allEvents) {
+    return res.status(404).json({ status: 404, message: 'No events found' });
+  }
+
+  if (method === 'POST') {
+    const { email, eventId } = req.body;
+    // we add our code here
+    const newAllEvents = allEvents.map((ev) => {
+      if (ev.id === eventId) {
+        if (ev.emails_registered.includes(email)) {
+          res
+            .status(409)
+            .json({ message: 'This email has already been registered' });
+          // console.log(res);
+          return ev;
+        }
+        return { ...ev, emails_registered: [...ev.emails_registered, email] };
+      }
+      return ev;
+    });
+    fs.writeFileSync(
+      filePath,
+      JSON.stringify({ events_categories, allEvents: newAllEvents }),
+    );
+    res.status(200).json({
+      message: `You have been registered to the event with id ${eventId} with email ${email}`,
+    });
+  }
 }
--- src/components/events/single-event.jsx
+++ src/components/events/single-event.jsx
@@ -1,29 +1,72 @@
-import React from 'react';
+import { useRef, useState } from 'react';
 import Image from 'next/image';
 import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
 
-export const SingleEvent = ({ data }) =>{
-  const onSubmit = (e) => {
-    alert(e.value);
-  }
+export const SingleEvent = ({ data }) => {
+  const inputEmail = useRef(null);
+  const router = useRouter();
+  const [message, setMessage] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    // console.log('router: ', router);
+    const emailValue = inputEmail.current.value;
+    const eventId = router?.query.id;
+
+    // email Regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(emailValue)) {
+      setMessage('Please insert a valid email');
+      return;
+    } else {
+      setMessage('');
+    }
+    // console.log('submitted')
+    try {
+      const response = await fetch('/api/email-registration', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: emailValue, eventId }),
+      });
+
+      if (!response.ok) throw new Error(`Error: ${response.status}`);
+      const data = await response.json();
+      console.log('POST', data);
+
+      // Post fetch request
+      // body emailValue, eventId
+    } catch (error) {
+      console.log('ERROR', error);
+    }
+  };
 
   return (
-  <div className='event_single_page'>
-    <Image
-      src={data.image}
-      width={1000}
-      height={300}
-      alt={data.title}
-      priority={true}
-    />
-    <h1>{data.title}</h1>
-    <p>{data.description}</p>
-    <form onSubmit={onSubmit} className='email_registration'>
-      <label htmlFor="email">Get Register</label>
-      <input type="email"  id="email" placeholder='Please insert your email here'/>
-      <button type='submit'>Submit</button>
-    </form>
-  </div>
-);} 
+    <div className="event_single_page">
+      <Image
+        src={data.image}
+        width={1000}
+        height={300}
+        alt={data.title}
+        priority={true}
+      />
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+      <form onSubmit={onSubmit} className="email_registration">
+        <label htmlFor="email">Get Register</label>
+        <input
+          ref={inputEmail}
+          type="email"
+          id="email"
+          placeholder="Please insert your email here"
+        />
+        <button type="submit">Submit</button>
+        <p>{message}</p>
+      </form>
+    </div>
+  );
+};
 
 SingleEvent.propTypes = { data: PropTypes.object.isRequired };
